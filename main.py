76005d8eb94ab887332493200d655ac83340766a
Nino Gralla <66381853+NinoKrb@users.noreply.github.com>
2022-01-09 09:28:21 -0500

    Reset score.json & fix directory loading
    Fix misloaded images due to file path resolution on Linux. It has now also been adapted for Windows systems.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import pygame, pygame.mixer
-import os, random, glob, time, json
+import pygame
+import pygame.mixer
+import os
+import random
+import glob
+import time
+import json
+
 
 class Settings():
     window_height = 645
     window_width = 1080
 
     base_file = os.path.dirname(os.path.abspath(__file__))
-    path_image = os.path.join(base_file, "assets/images/")
-    path_font = os.path.join(base_file, "assets/fonts/")
-    path_sounds = os.path.join(base_file, "assets/sounds/")
+    assets_folder = os.path.join(base_file, "assets")
+    path_image = os.path.join(assets_folder, "images")
+    path_font = os.path.join(assets_folder, "fonts")
+    path_sounds = os.path.join(assets_folder, "sounds")
     score_file = os.path.join(base_file, "score.json")
     title = "PopTheBubbles - The Game"
     fps = 60
+    scoreboard_count = 5
 
     plop_sound = 'plop_sound.wav'
     spawn_sound = 'spawn_sound.wav'
@@ -25,7 +33,7 @@ class Settings():
     gray_overlay_color = (77, 77, 77)
 
     # Fonts
-    font_type = "Pixellari.ttf" # ChubbyChoo-Regular
+    font_type = "Pixellari.ttf"  # ChubbyChoo-Regular
     font_info_size = 22
     font_overlay_size = 30
     font_title_size = 50
@@ -44,10 +52,11 @@ class Settings():
     text_play_time = "{}s"
     text_scoreboard = "Scoreboard"
     text_scoreboard_item = "Playtime: {}s, Score: {}, Popped Bubbles: {}"
+    text_scoreboard_empty = "There are no score entries available"
 
     # Bubble Settings
-    bubble_size = (5,5)
-    bubble_grow_speed = (1,4)
+    bubble_size = (5, 5)
+    bubble_grow_speed = (1, 4)
     bubble_grow_delay = 12.5
     max_grow_speed = 10
     bubble_spawn_delay = 60
@@ -59,7 +68,7 @@ class Settings():
     bubble_path = "bubbles/*.png"
     bubble_animation_frames = glob.glob(os.path.join(path_image, bubble_path))
 
-    bubble_collide_ratio = 4 
+    bubble_collide_ratio = 4
     # I have deliberately set the collide ratio higher here, because otherwise it could lead to almost impossible situations in the course of the game.
 
     # Images - Overlay Sprites
@@ -79,18 +88,22 @@ class Settings():
 class Background(pygame.sprite.Sprite):
     def __init__(self, filename):
         super().__init__()
-        self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert()
-        self.image = pygame.transform.scale(self.image, (Settings.window_width, Settings.window_height))
+        self.image = pygame.image.load(os.path.join(
+            Settings.path_image, filename)).convert()
+        self.image = pygame.transform.scale(
+            self.image, (Settings.window_width, Settings.window_height))
 
     def draw(self, screen):
-        screen.blit(self.image, (0,0))
+        screen.blit(self.image, (0, 0))
+
 
 class Bubble(pygame.sprite.Sprite):
     def __init__(self, filename):
         super().__init__()
         self.update_sprite(filename)
         self.rect = self.image.get_rect()
-        self.grow_speed = random.randint(Settings.bubble_grow_speed[0], Settings.bubble_grow_speed[1])
+        self.grow_speed = random.randint(
+            Settings.bubble_grow_speed[0], Settings.bubble_grow_speed[1])
 
         self.grow_speed = game.playtime // 2 * self.grow_speed * 0.1
         if self.grow_speed >= Settings.max_grow_speed:
@@ -108,7 +121,8 @@ class Bubble(pygame.sprite.Sprite):
         self.scale = {'width': self.rect.width, 'height': self.rect.height}
 
     def play_sound(self, file, channel):
-        self.sound = pygame.mixer.Sound(os.path.join(Settings.path_sounds, file))
+        self.sound = pygame.mixer.Sound(
+            os.path.join(Settings.path_sounds, file))
         pygame.mixer.Channel(channel).play(self.sound)
 
     def stop_sound(self):
@@ -117,13 +131,16 @@ class Bubble(pygame.sprite.Sprite):
 
     # Find spawn position
     def find_position(self):
-        self.rect.top = random.randrange(Settings.bubble_border_y + Settings.bubble_border_distance, Settings.window_height - Settings.bubble_border_y - self.rect.height - Settings.bubble_border_distance)
-        self.rect.left = random.randrange(Settings.bubble_border_x + Settings.bubble_border_distance, Settings.window_width - Settings.bubble_border_x - self.rect.width - Settings.bubble_border_distance)
-    
+        self.rect.top = random.randrange(Settings.bubble_border_y + Settings.bubble_border_distance,
+                                         Settings.window_height - Settings.bubble_border_y - self.rect.height - Settings.bubble_border_distance)
+        self.rect.left = random.randrange(Settings.bubble_border_x + Settings.bubble_border_distance,
+                                          Settings.window_width - Settings.bubble_border_x - self.rect.width - Settings.bubble_border_distance)
+
     # Check if spawn position is valid
     def check_spawn_position(self):
         for i in range(10):
-            hits = pygame.sprite.spritecollide(self, game.bubbles, False, pygame.sprite.collide_circle_ratio(Settings.bubble_collide_ratio))
+            hits = pygame.sprite.spritecollide(
+                self, game.bubbles, False, pygame.sprite.collide_circle_ratio(Settings.bubble_collide_ratio))
             if len(hits) > 0:
                 self.find_position()
                 continue
@@ -144,8 +161,10 @@ class Bubble(pygame.sprite.Sprite):
         self.check_collision()
 
     def update_sprite(self, filename):
-        self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
-        self.image = self.original_image = pygame.transform.scale(self.image, (Settings.bubble_size[0], Settings.bubble_size[1]))
+        self.image = pygame.image.load(os.path.join(
+            Settings.path_image, filename)).convert_alpha()
+        self.image = self.original_image = pygame.transform.scale(
+            self.image, (Settings.bubble_size[0], Settings.bubble_size[1]))
 
     # Scaling
     def scale_up(self):
@@ -159,7 +178,8 @@ class Bubble(pygame.sprite.Sprite):
     # Centering the Sprite
     def set_center(self):
         c = self.rect.center
-        self.image = pygame.transform.scale(self.original_image, (self.get_scale()))
+        self.image = pygame.transform.scale(
+            self.original_image, (self.get_scale()))
         self.rect = self.image.get_rect()
         self.rect.center = c
 
@@ -179,7 +199,8 @@ class Bubble(pygame.sprite.Sprite):
             self.play_sound(Settings.crash_sound, 3)
             game.save_score()
 
-        collisions = pygame.sprite.spritecollide(self, game.bubbles, False, pygame.sprite.collide_mask)
+        collisions = pygame.sprite.spritecollide(
+            self, game.bubbles, False, pygame.sprite.collide_mask)
         for sprite in collisions:
             if sprite != self:
                 game.game_over = True
@@ -192,13 +213,16 @@ class Bubble(pygame.sprite.Sprite):
         if self.death == True:
             if self.death_animation_count >= len(Settings.bubble_animation_frames):
                 self.play_sound(Settings.plop_sound, 2)
-                game.stats_points += round(self.scale['width'] / Settings.bubble_size[0] * 1)
+                game.stats_points += round(self.scale['width'] /
+                                           Settings.bubble_size[0] * 1)
                 self.kill()
                 return
 
-            self.update_sprite(Settings.bubble_animation_frames[self.death_animation_count].split("/")[-2] + "/" + Settings.bubble_animation_frames[self.death_animation_count].split("/")[-1])
+            self.update_sprite(Settings.bubble_animation_frames[self.death_animation_count].split(
+                "/")[-2] + "/" + Settings.bubble_animation_frames[self.death_animation_count].split("/")[-1])
             self.set_center()
             self.death_animation_count += 1
+
 
 class Cursor(pygame.sprite.Sprite):
     def __init__(self, filename):
@@ -220,14 +244,16 @@ class Cursor(pygame.sprite.Sprite):
             self.update_cursor(Settings.cursor_image_click)
         else:
             self.update_cursor(Settings.cursor_image_hand)
-    
+
     # Helper function to change the cursors image
     def update_cursor(self, filename):
-        self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
+        self.image = pygame.image.load(os.path.join(
+            Settings.path_image, filename)).convert_alpha()
         self.image = pygame.transform.scale(self.image, Settings.cursor_size)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
 
 class Game():
     def __init__(self):
@@ -237,7 +263,8 @@ class Game():
         pygame.init()
         pygame.display.set_caption(Settings.title)
 
-        self.screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
+        self.screen = pygame.display.set_mode(
+            (Settings.window_width, Settings.window_height))
         self.fps = pygame.time.Clock()
 
         # Define Sprites & Spritegroups
@@ -255,9 +282,12 @@ class Game():
         self.scoreboard = False
 
         # Set fonts for the game overlay
-        self.font = pygame.font.Font(os.path.join(Settings.path_font, Settings.font_type), Settings.font_overlay_size)
-        self.overlay_font = pygame.font.Font(os.path.join(Settings.path_font, Settings.font_type), Settings.font_title_size)
-        self.info_font = pygame.font.Font(os.path.join(Settings.path_font, Settings.font_type), Settings.font_info_size)
+        self.font = pygame.font.Font(os.path.join(
+            Settings.path_font, Settings.font_type), Settings.font_overlay_size)
+        self.overlay_font = pygame.font.Font(os.path.join(
+            Settings.path_font, Settings.font_type), Settings.font_title_size)
+        self.info_font = pygame.font.Font(os.path.join(
+            Settings.path_font, Settings.font_type), Settings.font_info_size)
 
         # Reset the game for a new session
         self.reset_game()
@@ -302,10 +332,11 @@ class Game():
     def update_overlay(self):
         if self.game_over or self.pause_menu or self.game_started:
             # Gray Overlay
-            pause_gray_overlay = pygame.Surface((Settings.window_width, Settings.window_height))
+            pause_gray_overlay = pygame.Surface(
+                (Settings.window_width, Settings.window_height))
             pause_gray_overlay.fill(Settings.gray_overlay_color)
             pause_gray_overlay.set_alpha(Settings.gray_overlay_alpha)
-            self.screen.blit(pause_gray_overlay, (0,0))
+            self.screen.blit(pause_gray_overlay, (0, 0))
 
             # Set/Render Overlay title
             if self.scoreboard:
@@ -317,37 +348,56 @@ class Game():
             elif self.game_started:
                 header_text = Settings.text_welcome_to
 
-            header_text = self.overlay_font.render(header_text, True, Settings.font_color_blue)
-            self.screen.blit(header_text, (Settings.window_width // 2 - header_text.get_rect().centerx, 25))
+            header_text = self.overlay_font.render(
+                header_text, True, Settings.font_color_blue)
+            self.screen.blit(header_text, (Settings.window_width //
+                             2 - header_text.get_rect().centerx, 25))
 
             # Load Logo
-            logo = pygame.image.load(os.path.join(Settings.path_image, Settings.logo_image)).convert_alpha()
+            logo = pygame.image.load(os.path.join(
+                Settings.path_image, Settings.logo_image)).convert_alpha()
             logo = pygame.transform.scale(logo, (604, 118))
-            self.screen.blit(logo, ((Settings.window_width // 2 - logo.get_rect().centerx), 75))
+            self.screen.blit(
+                logo, ((Settings.window_width // 2 - logo.get_rect().centerx), 75))
 
             # Render scoreboard
             if self.scoreboard:
                 item_count = 0
-                for item in self.load_scoreboard()[0:5]:
-                    item_count += 1
-                    scoreboard_item = self.font.render(Settings.text_scoreboard_item.format(item['playtime'], item['score'], item['popped_bubbles']), True, Settings.font_color_white)
-                    self.screen.blit(scoreboard_item, ((Settings.window_width // 2 - scoreboard_item.get_rect().centerx), ((item_count + 1) * (scoreboard_item.get_rect().height + 10)) + logo.get_rect().bottom))
+                scoreboard = self.load_scoreboard()
+                if len(scoreboard) > 0:
+                    for item in scoreboard[0:Settings.scoreboard_count]:
+                        item_count += 1
+                        scoreboard_item = self.font.render(Settings.text_scoreboard_item.format(
+                            item['playtime'], item['score'], item['popped_bubbles']), True, Settings.font_color_white)
+                        self.screen.blit(scoreboard_item, ((Settings.window_width // 2 - scoreboard_item.get_rect(
+                        ).centerx), ((item_count + 1) * (scoreboard_item.get_rect().height + 10)) + logo.get_rect().bottom))
+                else:
+                    scoreboard_item = self.font.render(
+                        Settings.text_scoreboard_empty, True, Settings.font_color_white)
+                    self.screen.blit(scoreboard_item, ((Settings.window_width // 2 - scoreboard_item.get_rect(
+                    ).centerx), ((2) * (scoreboard_item.get_rect().height + 10)) + logo.get_rect().bottom))
 
             else:
                 # Gameover Stats
                 if self.game_over or self.pause_menu:
                     # Score
-                    score_text = self.font.render(Settings.text_your_score.format(self.stats_points), True, Settings.font_color_white)
-                    self.screen.blit(score_text, ((Settings.window_width // 2 - score_text.get_rect().centerx), 75 + logo.get_rect().bottom))
+                    score_text = self.font.render(Settings.text_your_score.format(
+                        self.stats_points), True, Settings.font_color_white)
+                    self.screen.blit(score_text, ((
+                        Settings.window_width // 2 - score_text.get_rect().centerx), 75 + logo.get_rect().bottom))
 
                     # Playtime
-                    play_time_text = self.font.render(Settings.text_play_time.format(self.playtime), True, Settings.font_color_white)
-                    self.screen.blit(play_time_text, ((Settings.window_width // 2 - play_time_text.get_rect().centerx), 110 + logo.get_rect().bottom))
+                    play_time_text = self.font.render(Settings.text_play_time.format(
+                        self.playtime), True, Settings.font_color_white)
+                    self.screen.blit(play_time_text, ((
+                        Settings.window_width // 2 - play_time_text.get_rect().centerx), 110 + logo.get_rect().bottom))
 
                 if self.game_started or self.game_over:
                     # Loading "click to start" text
-                    click_to_start = pygame.image.load(os.path.join(Settings.path_image, Settings.click_to_start_image)).convert_alpha()
-                    click_to_start = pygame.transform.scale(click_to_start, (351, 85))
+                    click_to_start = pygame.image.load(os.path.join(
+                        Settings.path_image, Settings.click_to_start_image)).convert_alpha()
+                    click_to_start = pygame.transform.scale(
+                        click_to_start, (351, 85))
 
                     # Fade Animation for the "click to start" text
                     if self.alpha_counter >= 100:
@@ -361,23 +411,28 @@ class Game():
                         self.alpha_counter -= Settings.click_to_start_alpha_speed
 
                     click_to_start.set_alpha(self.alpha_counter)
-                    self.screen.blit(click_to_start, (Settings.window_width // 2 - click_to_start.get_rect().centerx, Settings.window_height // 2))
+                    self.screen.blit(click_to_start, (Settings.window_width // 2 -
+                                     click_to_start.get_rect().centerx, Settings.window_height // 2))
 
             # Render Keybinds info
-            keybinds_text = self.info_font.render(Settings.text_keybinds, True, Settings.font_color_overlay)
-            self.screen.blit(keybinds_text, (Settings.window_width // 2 - keybinds_text.get_rect().centerx, Settings.keybinds_label_margin))
-
+            keybinds_text = self.info_font.render(
+                Settings.text_keybinds, True, Settings.font_color_overlay)
+            self.screen.blit(keybinds_text, (Settings.window_width // 2 -
+                             keybinds_text.get_rect().centerx, Settings.keybinds_label_margin))
 
         else:
             # Hud/Stats Rendering
             # Render Points
-            points = self.font.render(Settings.text_points.format(self.stats_points), True, Settings.font_color_overlay)
+            points = self.font.render(Settings.text_points.format(
+                self.stats_points), True, Settings.font_color_overlay)
             self.screen.blit(points, Settings.points_label_margin)
 
             # Render Playtime
-            play_time = self.font.render(Settings.text_play_time.format(self.playtime), True, Settings.font_color_overlay)
+            play_time = self.font.render(Settings.text_play_time.format(
+                self.playtime), True, Settings.font_color_overlay)
             play_time_rect = play_time.get_rect()
-            play_time_rect.right = Settings.window_width - Settings.points_label_margin[0]
+            play_time_rect.right = Settings.window_width - \
+                Settings.points_label_margin[0]
             play_time_rect.top = Settings.points_label_margin[1]
             self.screen.blit(play_time, play_time_rect)
 
@@ -389,7 +444,8 @@ class Game():
             bubble.check_plop_animation()
 
         # Calculate the current playtime
-        self.playtime = int(round(time.time() - self.time - self.time_difference, 0))
+        self.playtime = int(
+            round(time.time() - self.time - self.time_difference, 0))
 
     # Playtimer mechanic
     def start_timer(self):
@@ -402,7 +458,7 @@ class Game():
         self.time_difference += time.time() - self.pause_time
 
     def end_timer(self):
-        self.time_total = time.time() - self.time - self.time_difference 
+        self.time_total = time.time() - self.time - self.time_difference
 
     # Reset the player stats
     def reset_stats(self):
@@ -428,13 +484,14 @@ class Game():
     # Load all stats from score file
     def load_scoreboard(self):
         file = open(Settings.score_file)
-        return json.load(file)['scores']
+        data = json.load(file)['scores']
+        return sorted(data, key=lambda x: x['score'], reverse=True)
 
     # Save players latest play score
     def save_score(self):
         file = open(Settings.score_file)
         data = json.load(file)
-        
+
         data['scores'].append({
             "playtime": self.playtime,
             "score": self.stats_points,
@@ -498,6 +555,7 @@ class Game():
                 # Close game
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
+
 
 if __name__ == '__main__':
     game = Game()
